a7e28af181d4c09812d0d8ea754f70c0c4777bd9
artchaidez <artchaidez1995@gmail.com>
2022-12-05 14:50:28 -0600

    Minor deletions
    Deleted Cypress

--- client/src/test/Challenge.test.js
+++ client/src/test/Challenge.test.js
@@ -10,7 +10,7 @@ import "@testing-library/jest-dom";
 
 jest.mock("axios", () => ({
     ...jest.requireActual("axios"),
-    post: jest.fn()
+    post: jest.fn(),
 }));
 
 afterEach(() =>{
@@ -36,7 +36,24 @@ describe("Challenge tests", () => {
 
     // https://www.youtube.com/watch?v=5nzoYEjvgRc
     // TODO: test useState handle: ClickReorderCost
-    test("Posts restock API correctly", async () => {
+    // npm test Challenge -t "Posts restock API correctly"
+    test.only("Posts restock API correctly", async () => {
 
-        });
+        // need to call handleClickReorderCost
+        //lowStock.data
+        // then calls setRestockCost(data.data);
+
+        // toHaveBeenCalledWith(url, data to get back)
+        /** name: "Good & Plenty",stock:4,capacity:20,cost:16,id:786123,
+                     name:"Twix",stock:17,capacity:70,cost:53,id:627791,
+                     name:"Starburst",stock:8,capacity:45,cost:37,id:506709,
+                     name:"Butterfinger",stock:10, capacity:60,cost:50,id:601091,
+                     name:"Sour Patch Kids",stock:14,capacity:60,cost:46,id:520745, */
+        //axios.post
+        /*expect(axios.post).toHaveBeenCalledWith("http://localhost:4567/restock-cost",
+         expect.objectContaining({
+         "capacity": 60, "cost": 46, "id": 520745, "name": "Sour Patch Kids", "stock": 14
+         })
+         );*/
+    });
 })
